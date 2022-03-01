12eaaec7e769c27444b69f15ab09f47b9c870a45
Ketan Pawar <98469648+Ketan-Pawar@users.noreply.github.com>
2022-03-01 13:41:37 +0530

    Object Class

---
+++ Practice/objectClass.js
@@ -0,0 +1,25 @@
+// console.log(Object.prototype.toString.call("String"))
+
+// (object.prototype.toString.call(42))
+
+// (object.prototype.toString.call(true))
+
+// (object.prototype.toString.call(true))
+
+// Object.prototype.toString.call(Object()) //or
+// Object.prototype.toString.call({})
+
+// Object.prototype.toString.call(function(){})
+
+// Object.prototype.toString.call(new Date(2015,10,21))
+
+// Object.prototype.toString.call(new RegExp()) //or
+// Object.prototype.toString.call(/foo/);
+
+// Object.prototype.toString.call([]);
+
+// Object.prototype.toString.call(null);
+
+// Object.prototype.toString.call(undefined);
+
+// Object.prototype.toString.call(Error());
