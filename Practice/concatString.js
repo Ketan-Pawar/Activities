1aea734b97de89fc1bb2bdfc7a96280cda5882a7
Ketan Pawar <98469648+Ketan-Pawar@users.noreply.github.com>
2022-03-01 13:41:07 +0530

    String Concatenation

---
+++ Practice/concatString.js
@@ -0,0 +1,22 @@
+// var foo = "Foo";
+// var bar = "Bar";
+// console.log(foo + bar);
+// console.log(foo + " " + bar);
+
+// var foo = "Foo";
+// var bar = "Bar";
+// console.log(foo.concat(bar));
+// console.log("a".concat("b", " ", "d"));
+
+// Info: Strings can be concatenated with non-string variables but will type-convert the non-string variables into strings.
+// var string = "string";
+// var number = 32;
+// var boolean = true;
+// console.log(string + number + boolean)
+
+// var place = `World`;
+// var greet = `Hello ${place}!`
+// console.log(greet)
+
+// `a\\b`
+// String.raw`a\\b`
