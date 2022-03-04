44a6aaa59e613a279c700c24e154da37d8f8abda
Ketan Pawar <98469648+Ketan-Pawar@users.noreply.github.com>
2022-03-04 11:53:55 +0530

    Reverse String

---
+++ Practice/stringReverse.js
@@ -0,0 +1,16 @@
+// function stringReverse(str){
+//     return str.split('').reverse('').join('');
+// };
+
+// console.log(stringReverse('GoLive'));
+// console.log(stringReverse("HinjewadiPhase1"))
+
+// function reverse(string) {
+//     var strRev = "";
+//     for (var i = string.length - 1; i >= 0; i--) {
+//     strRev += string[i];
+//     }
+//     return strRev;
+//    }
+
+// console.log(reverse('Live'));
