e40a713cc11661d10d8fd0631438aee2df8eda39
Ketan Pawar <98469648+Ketan-Pawar@users.noreply.github.com>
2022-03-04 11:50:04 +0530

    Access char

---
+++ Practice/accessChar.js
@@ -0,0 +1,23 @@
+// var string = "Hello World";
+// console.log( string.charAt(4));
+
+// var string = "Hello, World!";
+// console.log( string[4] ); 
+
+// var string = "Hello, World!";
+// console.log( string.charCodeAt(4) );
+
+// // Escaping Quotes
+// var text = "I feel \"high\"";
+// // console.log(text)
+
+
+// Trim Function
+// console.log("      Ketan Pawar".trim());
+
+// console.log("   this is me  ".trimStart());
+// console.log("  this is me  ".trimEnd());
+
+// console.log(" this is me ".trimLeft()); 
+// console.log(" this is me ".trimRight());
+
